59511c02e57c25a80380b25a0f2eefad562fcc9f
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-27 15:02:12 +0200

    add for loop

--- js/app-for.js
+++ js/app-for.js
@@ -1,1 +1,17 @@
-console.log('for loop');
+const listWrapperEl = document.querySelector('.list-wrapper');
+listWrapperEl.innerHTML = '';
+
+const shoppingList = [
+    'biscotti x2',
+    'acqua x6',
+    'pasta x2',
+    'carne',
+    'uova',
+    'formaggio',
+    'pizza x2'
+];
+
+for (let i = 0; i < shoppingList.length; i++) {
+    const listItem = shoppingList[i];
+    listWrapperEl.innerHTML += `<li>${listItem}</li>`;
+}
